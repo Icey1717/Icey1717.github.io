53e031c545802f7c84492ee1c36e6074f1ea9453
Icey1717 <icey1717@gmail.com>
2025-01-18 12:20:45 +1100

    Launch fireworks over time.

--- fireworks.js
+++ fireworks.js
@@ -8,5 +8,15 @@ fireworks.start()
 function triggerFireworks() {
     fireworks.updateSize({width: window.innerWidth, height: window.innerHeight});
     fireworks.updateBoundaries({x: 0, y: 0, width: window.innerWidth, height: window.innerHeight});
-    fireworks.launch(25);
+
+    let count = 50;
+
+    const interval = setInterval(() => {
+        fireworks.launch(1);
+        count--;
+
+        if (count === 0) {
+            clearInterval(interval);
+        }
+    }, 50);
 }
